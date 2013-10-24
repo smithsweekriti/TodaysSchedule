5144739975151639a28a20ebb7c3926948ad87ac
smithsweekriti <sweekritismith@gmail.com>
2013-10-24 15:38:00 +0500

    CaseTools Project

--- FINAL/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn=mysql_connect("localhost","root","");
-mysql_select_db("android",$conn);
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-
-?>
